b19fc5767fce141c83f4aa1ee4faff0b477a44b4
Владелец <@JOB>
2022-08-07 20:25:47 +0700

    Косметика

--- E-num/Properties/AssemblyInfo.cs
+++ E-num/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("E-num 0.0.3")]
+[assembly: AssemblyTitle("E-num")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("E-num 0.0.3")]
+[assembly: AssemblyProduct("E-num")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
